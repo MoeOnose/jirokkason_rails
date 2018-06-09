fdb467c9e48eb5bfa2f9154b11ab78b1a13012ca
mormorbump <mormorbump@gmail.com>
2018-06-10 03:05:48 +0900

    delete send id

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,7 +24,6 @@ class ArticlesController < ApplicationController
 
   def destroy
   	article = Article.find(params[:id])
-  	favorite = article.favorite
   	if article.password == params[:password]
   		article.delete
   		redirect_to root_path
